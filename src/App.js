ee7e193682e759b6dcaaa874d999740eef0fa418
Shalomnze <nzeugochukwu4@gmail.com>
2023-03-06 18:42:57 +0100

    React weather app using openweathermap api

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import hot from "./assets/hot.jpg";
+import cold from "./assets/cold.jpg";
+
+function App() {
+  return (
+    <div className="app" style={{backgroundImage: `url(${cold})`}}>
+      
+      <div className="overlay">
+
+        <div className="container">
+
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter city..." />
+               <button>°F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+                <h3>london, GB</h3>
+                <img src="#" alt="weather icon" />
+                <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+
+          {/* bottom description */}
+
+        </div>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
